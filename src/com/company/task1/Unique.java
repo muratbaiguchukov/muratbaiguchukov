eb103613433c51d7668b6e9b7aac628266d739f0
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-02-26 10:52:14 +0600

    add-exam-4-zad-1

---
+++ src/com/company/task1/Unique.java
@@ -0,0 +1,21 @@
+package com.company.task1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Unique {
+    public static void main(String[] args) {
+        String[] str1 = {"1, 2, 3, 4, 4, 5"};
+        ArrayList<String> arr = new ArrayList<>();
+        String[] strs1 = arr.toArray(new String[arr.size()]);
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("1");
+        hashSet.add("2");
+        hashSet.add("3");
+        hashSet.add("4");
+        hashSet.add("4");
+        hashSet.add("5");
+        System.out.println(hashSet);
+    }
+}
